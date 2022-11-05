9d09cd016365cd245b9e99ed695093b63cce8260
ali.bakhshoude <ali.bakhshoude@snapp.cab>
2022-11-05 10:55:41 +0100

    logic cleanup

--- getIP.go
+++ getIP.go
@@ -21,20 +21,6 @@ func getPublicIP() string {
 	return clientPublicIP
 }
 
-func getLatLon(ip string) (string, string) {
-	req, err := http.Get("http://ip-api.com/json/" + ip)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	res, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	lat := gjson.Get(string(res), "lat").String()
-	lon := gjson.Get(string(res), "lon").String()
-	return lat, lon
-}
-
 // func main() {
 // 	lat, lon := getLatLon(getPublicIP())
 // 	fmt.Println(lat, lon)
--- main.go
+++ main.go
@@ -2,25 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-
-	"github.com/tidwall/gjson"
 )
 
 func main() {
-	apiResponse, err := http.Get("https://api.open-meteo.com/v1/forecast?latitude=52.3738&longitude=4.8910&hourly=temperature_2m&current_weather=true")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	responseBody, err := ioutil.ReadAll(apiResponse.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	currentTemp := gjson.Get(string(responseBody), "current_weather.temperature")
 	lat, lon := getLatLon(getPublicIP())
-	fmt.Println(lat, lon)
-	fmt.Println("Current temprature is: ", currentTemp)
-
+	fmt.Println("Your Public address : ", getPublicIP(), "\n", "Current temprature : ", getWeather(lat, lon))
 }
